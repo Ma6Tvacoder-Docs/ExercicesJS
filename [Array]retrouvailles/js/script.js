8dae44a2e038bf3c782c5e8cb91dca8001d08316
jeremy TELGA <jeremy.telga@lepoles.com>
2016-11-16 17:06:32 +0100

    initialisation

---
+++ [Array]retrouvailles/js/script.js
@@ -0,0 +1,17 @@
+// Initialisation du document
+var chouwi = document.getElementById('character_01');
+
+// création des éléments d'affichage
+var picture = document.createElement("img");
+var title = document.createElement("h3");
+
+// On récupère la source de l'image
+var srcImage = data.perso_0.picture;
+var altText = data.perso_0.name;
+
+// Ici on applique tout cela à l'image 
+picture.src = srcImage;
+picture.alt = altText;
+
+chouwi.appendChild(title);
+chouwi.appendChild(picture);

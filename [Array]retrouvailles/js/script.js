fb91564110026c5aa390d8d43bcd2dc0f428ecee
adrien centonze <adrien centonze>
2016-11-17 08:54:54 +0100

    ajout des image en preparation pour futur exo

--- [Array]retrouvailles/js/script.js
+++ [Array]retrouvailles/js/script.js
@@ -2,25 +2,25 @@
 //Il nous faut, l'image du personne, son nom et des dialogues defilants.
 
 //On commence par creer les elements du premiers personnage
-function init(submit){
+function init(infos){
 
    //recuperation du context pour l'implementation des elements
-  if( submit == data.perso_0 ){
+  if( infos == data.perso_0 ){
     var contexte = document.getElementById('character_01')
   }else{
     var contexte = document.getElementById('character_02')
   }
   // Création des nouveaux éléments de paragraphe
-    var image  = document.createElement("img");
-    var name   = document.createElement("h3");
+    var portrait  = document.createElement("img");
+    var nom   = document.createElement("h3");
     var bouton = document.createElement("button");
     var parole = document.createElement("div");
   //implementation des elements à integrer :
     //Gestion de l'image
-    image.src= submit.picture;
+    portrait.src= submit.picture;
 
     //Affichage du nom
-    name.innerHTML = submit.name;
+    nom.innerHTML = submit.name;
 
     //Reglage du bouton
     bouton.innerHTML = 'Retrouvailles';
@@ -32,11 +32,13 @@ function init(submit){
     //console.log([bouton])
 
   //generation des elements dans le DOM
-    contexte.appendChild(name);
-    contexte.appendChild(image);
+    contexte.appendChild(nom);
+    contexte.appendChild(portrait);
     contexte.appendChild(parole)
 
-    if(document.getElementsByTagName('button').length == 0 ){
+    //Creation du bouton
+      //--> Avant d'incerer le bouton dans le DOM on test si il n'existe pas déjà
+    if( document.getElementsByTagName('button').length == 0 ){
       document.getElementsByTagName('body')[0].appendChild(bouton);
     }
 }
@@ -48,15 +50,16 @@ init(data.perso_1);
 var index = 0;
 
 function dialogue(){
-  if (index%2 == 0) {
+
+  // Le modulo par deux --> la valeur est pair ou impair 0 || 1
+  if ( index%2 == 0 ) {
     var contexte = document.getElementById('character_02');
-    var diction = data.perso_1.texte;
+    var paroles = data.perso_1.texte;
   }else{
     var contexte = document.getElementById('character_01');
-    var diction = data.perso_0.texte;
+    var paroles = data.perso_0.texte;
   }
-
-  var dom = contexte.getElementsByClassName('center')
-  dom[0].innerHTML = diction[index%3];
-  index++
+  var dom = contexte.getElementsByClassName('center'); //--> Renvoie un tableau
+  dom[0].innerHTML = paroles[ index%3 ];
+  index++;
 }

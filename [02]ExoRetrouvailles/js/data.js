1d5b0b3841cb0dd33689e8477d9c30ae1a41cc14
adricen <adrien.centonze@gmail.com>
2017-03-05 19:59:38 +0100

    création du fichier redme.md pour workshop

--- [02]ExoRetrouvailles/js/data.js
+++ [02]ExoRetrouvailles/js/data.js
@@ -1,12 +1,12 @@
 var data = {
   'perso_0' : {
     'name' : 'Chouwi',
-    'picture' : 'https://s-media-cache-ak0.pinimg.com/236x/29/73/b1/2973b1e486b022a664e570f40d67e0da.jpg',
+    'picture' : 'img/chouwi.jpg',
     'texte' : ['Brouuuuwhaaaaa','Drrrriiiitch','Hinnn Wouaaa']
   },
   'perso_1' : {
     'name' : 'Archibalde',
-    'picture' : 'https://s-media-cache-ak0.pinimg.com/originals/00/37/03/0037037f1590875493f413c1fdbd52b1.jpg',
+    'picture' : 'img/archi.jpg',
     'texte' : ['Grand père, c\'est moi!','J\'aime les pâttes moi aussi','Pff nimporte quoi!']
   }
 }
